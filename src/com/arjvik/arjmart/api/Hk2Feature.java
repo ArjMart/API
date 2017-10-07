749ab8ca28a76471110f560ebb1fbc30e4c10e15
Arjun Vikram <arjvik@gmail.com>
2017-10-06 22:17:00 -0500

    simplified dep-inj

--- src/com/arjvik/arjmart/api/Hk2Feature.java
+++ src/com/arjvik/arjmart/api/Hk2Feature.java
@@ -1,16 +1,29 @@
 package com.arjvik.arjmart.api;
 
+import javax.inject.Singleton;
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
 import javax.ws.rs.ext.Provider;
+
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+
+import com.arjvik.arjmart.api.item.ItemAttributeDAO;
+import com.arjvik.arjmart.api.item.ItemDAO;
+import com.arjvik.arjmart.api.item.JDBCItemAttributeDAO;
+import com.arjvik.arjmart.api.item.JDBCItemDAO;
 
 @Provider
 public class Hk2Feature implements Feature {
 
     @Override
     public boolean configure(FeatureContext context) {
-        context.register(new DAOBinder())
-        		.register(new ConnectionFactoryBinder());
+        context.register(new AbstractBinder(){
+        	@Override
+        	protected void configure() {
+        		bind(JDBCItemDAO.class).to(ItemDAO.class).in(Singleton.class);
+        		bind(JDBCItemAttributeDAO.class).to(ItemAttributeDAO.class).in(Singleton.class);
+        	}
+        });
         return true;
     }
 }
