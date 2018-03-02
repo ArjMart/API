fb5b9b70d9a939beec38a7d5529325eae54ac577
Arjun Vikram <arjvik@gmail.com>
2018-03-02 09:39:04 -0600

    Commit

--- src/com/arjvik/arjmart/api/JDBCAuthenticationDAO.java
+++ src/com/arjvik/arjmart/api/JDBCAuthenticationDAO.java
@@ -1,15 +1,33 @@
 package com.arjvik.arjmart.api;
 
 public class JDBCAuthenticationDAO implements AuthenticationDAO {
-
-	@Override
-	public int authenticate(String credentials) throws AuthenticationFailedException {
-		// TODO Auto-generated method stub
-		return 0;
+	
+	private ConnectionFactory connectionFactory;
+	
+	@Inject
+	public JDBCAuthenticationDAO(ConnectionFactory connectionFactory) {
+		this.connectionFactory = connectionFactory;
 	}
 
 	@Override
-	public void authorize(int userID, String role) throws AuthorizationFailedException {
+	public int authenticate(String credentials) throws AuthenticationFailedException, DatabaseException {
+		try (Connection connection = connectionFactory.getConnection()) {
+			PreparedStatement statement = connection.prepareStatement("select UserID from User where Email = ? and Password = ?");
+			statement.setString(1, email);
+			statement.setString(2, password);
+			ResultSet resultSet = statement.executeQuery();
+			if(resultSet.next()) {
+				return resultSet.getInt("UserID");
+			} else {
+				throw new AuthenticationFailedException();
+			}
+		}catch (SQLException e) {
+			throw new DatabaseException(e);
+		}
+	}
+
+	@Override
+	public void authorize(int userID, String role) throws AuthorizationFailedException, DatabaseException {
 		// TODO Auto-generated method stub
 
 	}
