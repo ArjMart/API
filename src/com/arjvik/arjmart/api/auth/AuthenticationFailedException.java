527f0ce15c8f2cd4314c5779c5bc52168210c97f
Arjun Vikram <arjvik@gmail.com>
2018-04-14 17:31:58 -0500

    couple of things...

--- src/com/arjvik/arjmart/api/auth/AuthenticationFailedException.java
+++ src/com/arjvik/arjmart/api/auth/AuthenticationFailedException.java
@@ -2,19 +2,14 @@ package com.arjvik.arjmart.api.auth;
 
 public class AuthenticationFailedException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public AuthenticationFailedException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public AuthenticationFailedException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 }
